9ad50553fb9874e74a4674803feb7ec840ea1b60
dlevear <dlevear@gmail.com>
2025-07-27 11:05:28 -0400

    Hooray I found the answer, document = desktop.getCurrentComponent(). I figured this out thanks to inspecting the source of an existing python extension (I think it was a CSV importer of some kind).

--- HelloWorld.py
+++ HelloWorld.py
@@ -22,70 +22,14 @@ class HelloWorld(unohelper.Base, XJobExecutor, XEventListener):
             uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
             "HelloWorld",
             "Hello World! After you click OK, I will write Hello world in cell A1").execute()
-        context = self.context
-        contextdir = " ".join(dir(context))
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Got context successfully. Its type is {}. Here is everything it can do: {}".format(type(context), contextdir)).execute()
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Here are my globals: {}".format(str(globals()))).execute()
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Here are my locals: {}".format(str(locals()))).execute()
-        document = self.document
-        documentdir = " ".join(dir(document))
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Got document successfully. Its type is {}. Here is everything it can do: {}".format(type(document), documentdir)).execute()
-        desktop = self.desktop
-        desktopdir = " ".join(dir(desktop))
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Got desktop successfully. Its type is {}. Here is everything it can do: {}".format(type(desktop), desktopdir)).execute()
-        controller = desktop.Controller
-        controllerdir = " ".join(dir(controller))
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Got controller successfully. Its type is {}. Here is everything it can do: {}".format(type(controller), controllerdir)).execute()
-        sheet = document.getSheets()[0]
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Got first sheet successfully").execute()
+        document = self.desktop.getCurrentComponent()
+        sheet = document.getCurrentController().ActiveSheet
         sheet.getCellByPosition(0,0).setString("Hello world")
-        window.Toolkit.createMessageBox(
-            window,
-            uno.Enum('com.sun.star.awt.MessageBoxType', 'WARNINGBOX'),
-            uno.getConstantByName("com.sun.star.awt.MessageBoxButtons.BUTTONS_OK"),
-            "HelloWorld",
-            "Set cell successfully").execute()
         return
     # boilerplate code below this point
     def __init__(self, context):
         self.context = context
         # see https://api.libreoffice.org/docs/idl/ref/servicecom_1_1sun_1_1star_1_1frame_1_1Desktop.html
-        self.document = self.createUnoService("com.sun.star.frame.Document")
         self.desktop = self.createUnoService("com.sun.star.frame.Desktop")
         # see https://api.libreoffice.org/docs/idl/ref/servicecom_1_1sun_1_1star_1_1frame_1_1DispatchHelper.html
         self.dispatchhelper = self.createUnoService("com.sun.star.frame.DispatchHelper")
